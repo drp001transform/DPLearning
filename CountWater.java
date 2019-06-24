609f4e837eba0eb94e8906d3d3ecfc778c150c2b
drp001transform <30299587+drp001transform@users.noreply.github.com>
2019-06-24 13:42:45 +0100

    Add files via upload

--- CountWater.java
+++ CountWater.java
@@ -3,24 +3,27 @@ public class CountWater {
     public int measureWater(int mapProfile [] )  {
 
         int[] potentialWater;
-        potentialWater = new int[10];
+        potentialWater = new int[10]; // User to track squares that may contain water - each cell represents a horizontal level
         int currentMaxHeight = 0;
         int trappedWater = 0;
-
+//
+//  walk through each column - as you go keep track of the maximum column height and accumulating
+//  what could be water or air up to that height
+//
         for(int thiscolumnHeight: mapProfile)
-            { int depth = 1;
-                while(depth<=thiscolumnHeight)
-                { trappedWater=trappedWater+potentialWater[depth];
-                    potentialWater[depth]=0;
-                    depth++;
-                }
-                if (thiscolumnHeight < currentMaxHeight) {
+            { if (thiscolumnHeight < currentMaxHeight) {
                     for (int depthAbove = thiscolumnHeight + 1; depthAbove <= currentMaxHeight; depthAbove++) {
                         potentialWater [depthAbove]++;
                     }
                 }
                 else {
                     currentMaxHeight = thiscolumnHeight;
+                }
+                int depth = 1;
+                while(depth<=thiscolumnHeight)
+                { trappedWater=trappedWater+potentialWater[depth];
+                    potentialWater[depth]=0;
+                    depth++;
                 }
             }
         return trappedWater;
